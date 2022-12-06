a68a6732771b3087d4954a62010e10119bb170a5
Gizem Pesen <gizempesen@posta.mu.edu.tr>
2022-12-06 11:46:35 +0300

    Update jitsi.sh

--- jitsi.sh
+++ jitsi.sh
@@ -35,8 +35,13 @@ sudo ufw allow 443/tcp
 sudo ufw allow 4443/tcp
 sudo ufw allow 10000/udp
 sudo ufw allow 22/tcp
+#turnserver
 sudo ufw allow 3478/udp
 sudo ufw allow 5349/tcp
+#external jibri service
+sudo ufw allow 5222/tcp
+
+
 
 echo deb http://packages.prosody.im/debian $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list
 wget https://prosody.im/files/prosody-debian-packages.key -O- | sudo apt-key add -
